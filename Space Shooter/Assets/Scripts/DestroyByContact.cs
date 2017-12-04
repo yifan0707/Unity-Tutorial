e675739615b15c414b68c10c8f1ede54b53b17d0
Yifan Lu <lvan0707@outlook.com>
2017-12-04 17:31:55 +1000

    space shooter online tutorial part two finished

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    public GameObject playerExplosion;
+    public GameObject asteroidExplosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        else {
+            Instantiate(asteroidExplosion, this.transform.position, this.transform.rotation);
+            if (other.CompareTag("Player"))
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject asteroid;
+    public float width, height,spawnWait,waveWait;
+    public int asteroidCount;
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine (spawnWaves());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    IEnumerator spawnWaves() {
+        while (true) {
+            for (int i = 0; i < asteroidCount; i++)
+            {
+                Debug.Log("hazard created");
+                spawnPosition = new Vector3(Random.Range(-width, width), 0, height);
+                spawnRotation = Quaternion.identity;
+                Instantiate(asteroid, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
